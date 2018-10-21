a2df0b7ab86fe0b3bbce729a37b7677cbf8498ae
gjergjk71 <gjergjk71@gmail.com>
2018-10-21 12:23:49 +0200

    Created test_issue_with_config_exception method

--- tests/test_views.py
+++ tests/test_views.py
@@ -14,6 +14,11 @@ class TestViews(unittest.TestCase):
 	def test_200(self):
 		response = self.app.get("/")
 		self.assertEqual(response.status_code,200)
+	def test_issue_with_config_exception(self):
+		data = {"text":"print(1)"}
+		with self.assertRaises(Exception) as context:
+			response = self.app.post("/check_code",data=data)
+		self.assertTrue("Issue with pylint configuration" in str(context.exception))
 
 if __name__ == '__main__':
     unittest.main()
